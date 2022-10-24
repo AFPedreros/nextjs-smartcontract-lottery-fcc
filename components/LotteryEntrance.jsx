a03adc77d7721d0ea70cd78776160d4a7d39b3ce
AFPedreros <felipe.pedreros94@gmail.com>
2022-10-24 11:08:10 -0500

    making the lottery entrance

--- components/LotteryEntrance.jsx
+++ components/LotteryEntrance.jsx
@@ -1,13 +1,17 @@
 import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddress } from "../constants"
+import { useMoralis } from "react-moralis"
 
 export default function LotteryEntrance() {
-    const { runContractFunction: enterLottery } = useWeb3Contract({
-        abi: "",
-        contractAddress: "",
-        functionName: "",
-        params: {},
-        msgValue: "",
-    })
+    const { chainId: chainIdHex } = useMoralis()
+    console.log(parseInt(chainIdHex))
+    // const { runContractFunction: enterLottery } = useWeb3Contract({
+    //     abi: abi,
+    //     contractAddress: contractAddress,
+    //     functionName: "enterLottery",
+    //     params: {},
+    //     msgValue: "",
+    // })
 
     return <div>Lottery Entrance</div>
 }
